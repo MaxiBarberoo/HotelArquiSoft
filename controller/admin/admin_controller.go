f340aa8da5701be0cdbc07843c6bd6c71bfe113f
Mussa212 <santimussa21@gmail.com>
2023-05-30 14:52:31 -0300

    avances controller admin

--- controller/admin/admin_controller.go
+++ controller/admin/admin_controller.go
@@ -1,1 +1,59 @@
 package controller
+
+import (
+	"HotelArquiSoft/dto"
+	service "mvc-go/services"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+)
+
+func GetAdmin(c *gin.Context) {
+	log.Debug("Admin id to load: " + c.Param("id"))
+
+	id, _ := strconv.Atoi(c.Param("id"))
+	var adminDto dto.AdminDetailDto
+
+	adminDto, err := service.AdminService.GetAdminById(id)
+
+	if err != nil {
+		c.JSON(err.Status(), err)
+		return
+	}
+	c.JSON(http.StatusOK, adminDto)
+}
+
+func GetAdmins(c *gin.Context) {
+	var adminsDto dto.AdminsDto
+	adminsDto, err := service.AdminService.GetAdmins()
+
+	if err != nil {
+		c.JSON(err.Status(), err)
+		return
+	}
+
+	c.JSON(http.StatusOK, adminsDto)
+}
+
+func AdminInsert(c *gin.Context) {
+	var adminDto dto.AdminDto
+	err := c.BindJSON(&adminDto)
+
+	// Error Parsing json param
+	if err != nil {
+		log.Error(err.Error())
+		c.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	adminDto, er := service.AdminService.InsertAdmin(adminDto)
+	// Error del Insert
+	if er != nil {
+		c.JSON(er.Status(), er)
+		return
+	}
+
+	c.JSON(http.StatusCreated, adminDto)
+}
