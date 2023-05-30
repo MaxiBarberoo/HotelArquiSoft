76dedb173be50b4e30e79bf7239e639795d794da
Antonio <2100542@ucc.edu.ar>
2023-05-30 14:42:58 -0300

    Cambios en user

--- clients/admin/admin_client.go
+++ clients/admin/admin_client.go
@@ -1,1 +1,34 @@
 package clients
+
+import (
+	"HotelArquiSoft/model"
+	"github.com/jinzhu/gorm"
+	log "github.com/sirupsen/logrus"
+
+)
+var Db *gorm.DB
+
+func  GetAdminById(id int) model.Admin{
+	var admin model.Admin
+
+	Db.Where("id = ?", id).First.(&admin)
+	log.Debug("Admin: ",admin)
+	return admin
+}
+
+
+func GetAdmins() model.Admin {
+	var admins model.Admin
+	Db.Find(&admins)
+	log.Debug("Admins: ",admins)
+	return admins
+}
+
+func InsertAdmin(admin model.Admin) model.Admin{
+	result:= Db.Create(&admin)
+	if(result.Error != nil){
+		log.Error("")
+	}
+	log.Debug("Admin Created: ",admin.Id)
+	return admin
+}
--- services/user_service.go
+++ services/user_service.go
@@ -1,1 +1,68 @@
 package services
+
+import (
+	userClient "HotelArquiSoft/clients/user"
+	"HotelArquiSoft/dto"
+	e "HotelArquiSoft/utils"
+)
+
+type userService struct{}
+
+type userServiceInterface interface {
+	GetUserById(id int) (dto.UserDto, e.ApiError)
+	GetUsers() (dto.UsersDto, e.ApiError)
+	InsertUser(userDto dto.UserDto) (dto.UserDto, e.ApiError)
+}
+
+var (
+	UserService userServiceInterface
+)
+
+func init() {
+	UserService = &userService{}
+}
+
+func (s *userService) GetUserById(id int) (dto.UserDto, e.ApiError) {
+
+	var user model.User = userClient.GetUserById(id)
+	var userDto dto.UserDto
+
+	if user.Id == 0 {
+		return userDto, e.NewBadRequestApiError("user not found")
+	}
+
+	userDto.Name = user.Name
+	userDto.LastName = user.LastName
+
+	return userDto, nil
+}
+
+func (s *userService) GetUsers() (dto.UsersDto, e.ApiError) {
+
+	var users model.Users = userCliente.GetUsers()
+	var usersDto dto.UsersDto
+
+	for _, user := range users {
+		var userDto dto.UserDto
+		userDto.Name = user.Name
+		userDto.LastName = user.LastName
+		userDto.Id = user.Id
+
+		usersDto = append(usersDto, userDto)
+	}
+
+	return usersDto, nil
+}
+
+func (s *userService) InsertUser(userDto dto.UserDto) (dto.UserDto, e.ApiError) {
+
+	var user model.User
+
+	user.Name = userDto.Name
+	user.LastName = userDto.LastName
+	user.Password = userDto.Password
+
+	userDto.Id = user.Id
+
+	return userDto, nil
+}
