5c1ccab005beed33775efd4e9fb9f9c2a8298e90
Maximo Barbero <mbarbero48@gmial.com>
2023-05-30 15:17:44 -0300

    go mod init

---
+++ App/url_mappings.go
@@ -0,0 +1,17 @@
+package app
+
+import (
+	userController "mvc-go/controllers/user"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func mapUrls() {
+
+	// Users Mapping
+	router.GET("/user/:id", userController.GetUserById)
+	router.GET("/user", userController.GetUsers)
+	router.POST("/user", userController.UserInsert)
+
+	log.Info("Finishing mappings configurations")
+}
