228bb09fdc9d5c9d646205be451ea9294f245d31
Mussa212 <santimussa21@gmail.com>
2023-05-30 14:35:43 -0300

    client user

--- clients/admin/admin_client.go
+++ clients/admin/admin_client.go
@@ -1,1 +1,35 @@
 package clients
+
+import (
+	"HotelArquiSoft/model"
+	log "github.com/sirupsen/logrus"
+)
+
+//var Db *gorm.DB
+
+func GetAdminById(id int) model.Admin {
+	var admin model.Admin
+
+	Db.Where("id=?", id).First(&admin)
+	log.Debug("Admin: ", admin)
+
+	return admin
+}
+
+func GetAdmins() model.Admin {
+	var admin model.Admin
+	Db.Find(&admin)
+	log.Debug("Admin: ", admin)
+	return admin
+}
+
+func InsertAdmin(admin model.Admin) model.Admin {
+	result := Db.Create(&admin)
+
+	if result.Error != nil {
+		//TODO Manage Errors
+		log.Error("")
+	}
+	log.Debug("Admin Created: ", admin.Id)
+	return admin
+}
--- clients/user/user_client.go
+++ clients/user/user_client.go
@@ -1,1 +1,37 @@
 package clients
+
+package clients
+
+import (
+"HotelArquiSoft/model"
+log "github.com/sirupsen/logrus"
+)
+
+//var Db *gorm.DB
+
+func GetUserById(id int) model.User {
+	var user model.User
+
+	Db.Where("id=?", id).First(&user)
+	log.Debug("User: ", user)
+
+	return user
+}
+
+func GetUsers() model.User {
+	var user model.User
+	Db.Find(&user)
+	log.Debug("User: ", user)
+	return user
+}
+
+func InsertUser(user model.User) model.User {
+	result := Db.Create(&user)
+
+	if result.Error != nil {
+		//TODO Manage Errors
+		log.Error("")
+	}
+	log.Debug("User Created: ", user.Id)
+	return user
+}
