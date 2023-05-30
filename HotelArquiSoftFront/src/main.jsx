1a4e5d83211191c33cf140cae48a555e62fe2cb1
orne <2115318@ucc.edu.ar>
2023-05-30 11:06:38 -0300

    Modificaciones front

--- HotelArquiSoftFront/src/main.jsx
+++ HotelArquiSoftFront/src/main.jsx
@@ -1,10 +1,14 @@
+//punto de entrada a la aplicacion
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import {App} from "../HotelArquiSoftFront/src/App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+/*import App from './App.jsx' //importo el componente app
+import './index.css'*/
+
+/*const root*/ ReactDOM.createRoot(document.getElementById('root')).render(
+
     <App />
-  </React.StrictMode>,
+
 )
