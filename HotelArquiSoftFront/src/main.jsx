98eba560ee61e7334da197c9dff1fddcdf79b4f8
juanmiron <2100815@ucc.edu.ar>
2023-06-06 14:20:26 -0300

    Front renovado

--- HotelArquiSoftFront/src/main.jsx
+++ HotelArquiSoftFront/src/main.jsx
@@ -1,13 +1,10 @@
-//punto de entrada a la aplicacion
-
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {App} from "./App.jsx";
+import App from './App.jsx'
+import './index.css'
 
-/*import App from './App.jsx' //importo el componente app
-import './index.css'*/
-
-/*const root*/ ReactDOM.createRoot(document.getElementById('root')).render(
-
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-
+  </React.StrictMode>,
 )
