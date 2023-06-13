77757a01278c20a8a23224cc516dd87a47c3a463
Mussa212 <santimussa21@gmail.com>
2023-06-12 22:48:55 -0300

    Anda todo hasta reservas en front

--- HotelArquiSoftFront/src/App.jsx
+++ HotelArquiSoftFront/src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import './App.css'
 import BotonLogin from './Componentes/BotonLogin';
 import BotonRegister from './Componentes/BotonRegister';
 import Hoteles from './Componentes/Hoteles.jsx';
@@ -7,7 +6,7 @@ import Hoteles from './Componentes/Hoteles.jsx';
 function App() {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [isAdmin, setIsAdmin] = useState(false);
-    const [hotels, setHotels] = useState([]);
+    const [hoteles, setHoteles] = useState([]);
     const [userId, setUserId] = useState(null);
 
     const handleLogin = (tipoUsuario, userId) => {
@@ -16,24 +15,32 @@ function App() {
         setUserId(userId);
     };
 
+    useEffect(() => {
+        fetch('http://localhost:8090/hotels')
+            .then(response => response.json())
+            .then(data => setHoteles(data))
+            .catch(error => console.error(error));
+    }, []);
+
     return (
-    <div className = 'App'>
-        <div>
-           <h1>ENCONTRA LA MEJOR OPCION</h1>
-           {!isLoggedIn && <BotonLogin handleLogin={handleLogin} />}
-           {!isLoggedIn && <BotonRegister />}
-           {!isAdmin && hotels.map(hotel => (
-          <Hoteles
-            key={hotel.id}
-            nombreHotel={hotel.name}
-            piezas={hotel.cantHabitaciones}
-            isLoggedIn={isLoggedIn}
-            hotelId={hotel.id}
-            userId={userId}
-          />
-        ))}
+        <div className="App">
+            <div>
+                <h1>ENCONTRA LA MEJOR OPCION</h1>
+                {!isLoggedIn && <BotonLogin handleLogin={handleLogin} />}
+                {!isLoggedIn && <BotonRegister />}
+                {!isAdmin && hoteles.map(hotel => (
+                    <Hoteles
+                        key={hotel.id}
+                        nombreHotel={hotel.name}
+                        piezas={hotel.cantHabitaciones}
+                        isLoggedIn={isLoggedIn}
+                        hotelId={hotel.id}
+                        userId={userId}
+                    />
+                ))}
+            </div>
         </div>
-    </div>
-    )
+    );
 }
+
 export default App;
