c9da642f78f7cc0dfd9e16ed45c995597db5722a
Juanmairon <2100815@ucc.edu.ar>
2023-06-13 00:24:45 -0300

    Prueba 1 de reservas con nombre de hotel

--- HotelArquiSoftFront/src/Componentes/Reservas.jsx
+++ HotelArquiSoftFront/src/Componentes/Reservas.jsx
@@ -1,24 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Reservas(props) {
-    const { reservas } = props;
+    const [reservas, setReservas] = useState([]);
+
+    useEffect(() => {
+        // Llamada al método getReservasByUser utilizando fetch
+        fetch(`http://localhost:8090/reservas/reservauser/${props.userId}`)
+          .then((response) => response.json())
+          .then((data) => {
+            // Actualizar el estado con las reservas obtenidas
+            setReservas(data);
+    
+            // Obtener el nombre del hotel para cada reserva
+            const fetchHotelName = async () => {
+              const reservasConHotel = await Promise.all(
+                data.map(async (reserva) => {
+                  const response = await fetch(
+                    `http://localhost:8090/hotels/${reserva.hotel_id}`
+                  );
+                  const hotelData = await response.json();
+                  return { ...reserva, hotel_nombre: hotelData.name };
+                })
+              );
+              setReservas(reservasConHotel);
+            };
+    
+            fetchHotelName();
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+          });
+      }, [props.userId]);
 
     return (
         <div>
             <h2>Reservas</h2>
-            {reservas.length > 0 ? (
-                <ul>
-                    {reservas.map((reserva) => (
-                        <li key={reserva.id}>
-                            <p>Fecha de ingreso: {reserva.fecha_ingreso}</p>
-                            <p>Fecha de egreso: {reserva.fecha_egreso}</p>
-                            <p>Hotel ID: {reserva.hotel_id}</p>
-                        </li>
-                    ))}
-                </ul>
-            ) : (
-                <p>No tienes reservas realizadas.</p>
-            )}
+            {reservas.map((reserva) => (
+        <div key={reserva.id}>
+          <p>Fecha de Ingreso: {reserva.fecha_ingreso}</p>
+          <p>Fecha de Egreso: {reserva.fecha_egreso}</p>
+          <p>Hotel: {reserva.hotel_nombre}</p>
+        </div>
+      ))}
         </div>
     );
 }
