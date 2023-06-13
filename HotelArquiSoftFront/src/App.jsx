acfa0b0d1e63041df5857f54711d22e68b4e4f82
Juanmairon <2100815@ucc.edu.ar>
2023-06-13 01:40:53 -0300

    Prueba admin

--- HotelArquiSoftFront/src/App.jsx
+++ HotelArquiSoftFront/src/App.jsx
@@ -11,6 +11,48 @@ function App() {
   const [userId, setUserId] = useState(null);
   const [mostrarReservas, setMostrarReservas] = useState(false);
   const [reservas, setReservas] = useState([]);
+  const [nuevoHotel, setNuevoHotel] = useState({
+    nombre: "",
+    cantidadHabitaciones: 0
+  });
+
+  const handleNuevoHotelChange = (event) => {
+    const { name, value } = event.target;
+    setNuevoHotel((prevNuevoHotel) => ({
+      ...prevNuevoHotel,
+      [name]: value
+    }));
+  };
+
+  const crearNuevoHotel = () => {
+    fetch("http://localhost:8090/hotels", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name: nuevoHotel.nombre,
+        cantHabitaciones: nuevoHotel.cantidadHabitaciones
+      })
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Verificar si se produjo un error en la respuesta
+        if (data.error) {
+          // Manejar el error, mostrar mensaje, etc.
+          console.error(data.error);
+        } else {
+          // Actualizar la lista de hoteles con el nuevo hotel agregado
+          setHoteles((prevHoteles) => [...prevHoteles, data]);
+          // Reiniciar los valores del nuevo hotel
+          setNuevoHotel({
+            nombre: "",
+            cantidadHabitaciones: 0
+          });
+        }
+      })
+      .catch((error) => console.error(error));
+  };
 
   const handleLogin = (tipoUsuario, userId) => {
     setIsLoggedIn(true);
@@ -52,6 +94,26 @@ function App() {
                 <button onClick={toggleReservas}>Reservas</button>
                 {mostrarReservas && <Reservas reservas={reservas} />}
               </div>)}
+              {isAdmin && (
+              <div>
+                <h2>Agregar nuevo hotel</h2>
+                <input
+                  type="text"
+                  name="nombre"
+                  value={nuevoHotel.nombre}
+                  onChange={handleNuevoHotelChange}
+                  placeholder="Nombre del hotel"
+                />
+                <input
+                  type="number"
+                  name="cantidadHabitaciones"
+                  value={nuevoHotel.cantidadHabitaciones}
+                  onChange={handleNuevoHotelChange}
+                  placeholder="Cantidad de habitaciones"
+                />
+                <button onClick={crearNuevoHotel}>Crear Hotel</button>
+              </div>
+              )}
           {!isAdmin && hoteles.map(hotel => (
               <Hoteles
                   key={hotel.id}
--- HotelArquiSoftFront/src/Componentes/Reservas.jsx
+++ HotelArquiSoftFront/src/Componentes/Reservas.jsx
@@ -2,42 +2,13 @@ import React, { useEffect, useState } from "react";
 
 function Reservas(props) {
   const { reservas } = props;
-  const [reservasConNombreHotel, setReservasConNombreHotel] = useState([]);
-
-  useEffect(() => {
-    const fetchReservasConNombreHotel = async () => {
-      const reservasConNombre = await Promise.all(
-        reservas.map(async (reserva) => {
-          const hotel = await getHotelById(reserva.hotel_id);
-          return { ...reserva, hotel_nombre: hotel.name };
-        })
-      );
-      setReservasConNombreHotel(reservasConNombre);
-    };
-
-    fetchReservasConNombreHotel();
-  }, [reservas]);
-
-  const getHotelById = async (hotelId) => {
-    try {
-      const response = await fetch(`http://localhost:8090/hotels/${hotelId}`);
-      if (!response.ok) {
-        throw new Error("Error al obtener el hotel");
-      }
-      const hotel = await response.json();
-      return hotel;
-    } catch (error) {
-      console.error("Error:", error);
-      return null;
-    }
-  };
 
   return (
     <div>
       <h2>Reservas</h2>
-      {reservasConNombreHotel.length > 0 ? (
+      {reservas.length > 0 ? (
         <ul>
-          {reservasConNombreHotel.map((reserva) => (
+          {reservas.map((reserva) => (
             <li key={reserva.id}>
               <p>Fecha de ingreso: {reserva.fecha_ingreso}</p>
               <p>Fecha de egreso: {reserva.fecha_egreso}</p>
