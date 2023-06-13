ef28679612f1337fc33e5ca89be274dd7a637d8e
Juanmairon <2100815@ucc.edu.ar>
2023-06-13 02:54:31 -0300

    QUE ANDEEEEEEEEEEEE

--- HotelArquiSoftFront/src/Componentes/Reservas.jsx
+++ HotelArquiSoftFront/src/Componentes/Reservas.jsx
@@ -1,43 +1,42 @@
-import React, {useEffect, useState} from "react";
+import React from 'react';
 
 function Reservas(props) {
-    const { reservas } = props;
-    const [reservasTotales, setReservasTotales] = useState([]);
-    
-    return (
-        <div>
-            <h2>Reservas</h2>
-            {reservas.length > 0 ? (
-                <ul>
-                    {reservas.map((reserva) => (
-                        <li key={reserva.id}>
-                            <p>Fecha de ingreso: {reserva.fecha_ingreso}</p>
-                            <p>Fecha de egreso: {reserva.fecha_egreso}</p>
-                            <p>Hotel: {reserva.hotel_id}</p>
-                        </li>
-                    ))}
-                </ul>
-            ) : (
-                <p>No tienes reservas realizadas.</p>
-            )}
+  const { reservas, reservasTotales } = props;
 
-            {reservasTotales.length > 0 && (
-                <div>
-                    <h3>Todas las reservas</h3>
-                    <ul>
-                        {reservasTotales.map((reserva) => (
-                            <li key={reserva.id}>
-                                <p>Usuario: {reserva.usuario}</p>
-                                <p>Fecha de ingreso: {reserva.fecha_ingreso}</p>
-                                <p>Fecha de egreso: {reserva.fecha_egreso}</p>
-                                <p>Hotel: {reserva.hotel_id}</p>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            )}
+  return (
+    <div>
+      <h2>Reservas</h2>
+      {reservas.length > 0 ? (
+        <ul>
+          {reservas.map((reserva) => (
+            <li key={reserva.id}>
+              <p>Fecha de ingreso: {reserva.fecha_ingreso}</p>
+              <p>Fecha de egreso: {reserva.fecha_egreso}</p>
+              <p>Hotel: {reserva.hotel_id}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No tienes reservas realizadas.</p>
+      )}
+
+      {reservasTotales.length > 0 && (
+        <div>
+          <h3>Todas las reservas</h3>
+          <ul>
+            {reservasTotales.map((reserva) => (
+              <li key={reserva.id}>
+                <p>Usuario: {reserva.user_id}</p>
+                <p>Fecha de ingreso: {reserva.fecha_ingreso}</p>
+                <p>Fecha de egreso: {reserva.fecha_egreso}</p>
+                <p>Hotel: {reserva.hotel_id}</p>
+              </li>
+            ))}
+          </ul>
         </div>
-    );
+      )}
+    </div>
+  );
 }
 
 export default Reservas;
