c418b8b8854d07dea7291cabbe4d912233719b76
Juanmairon <2100815@ucc.edu.ar>
2023-07-06 15:45:46 -0300

    Front arreglado

--- FrontHotel/src/Rutas/Home.jsx
+++ FrontHotel/src/Rutas/Home.jsx
@@ -2,32 +2,6 @@ import React from "react"
 import '../Stylesheet/Home.css'
 import Header from '../Componentes/Header'
 
-<<<<<<< HEAD
-function Home(){
-  return (
-
-      <div className="header">
-
-          <h1>SUMMIT LUXURY Hotels</h1>
-
-          <div className="contenedor-boton">
-              <button className="boton-uno"><span><strong> INGRESA PARA HACER TUS RESERVAS! </strong></span></button>
-
-          </div>
-
-          <div className="contenedor-hoteles">
-
-              <div className="contenedor-detalle-hoteles">
-                  <p className="nombre-hotel1"></p>
-
-              </div>
-
-          </div>
-      </div>
-
-      );
-=======
-
 function Home() {
     return(
         <div className="header">
@@ -42,7 +16,6 @@ function Home() {
             </div>
         </div>
     );
->>>>>>> b6bd42cc3d0507138cf5a276f6623231bdc05af7
 }
 
 export default Home;
