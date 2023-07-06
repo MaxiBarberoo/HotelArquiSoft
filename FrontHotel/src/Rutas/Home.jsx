908118287d558f57cf65ff9fda1b82e0135bc09c
orne <2115318@ucc.edu.ar>
2023-07-06 01:03:42 -0300

    Agrego css

--- FrontHotel/src/Rutas/Home.jsx
+++ FrontHotel/src/Rutas/Home.jsx
@@ -2,13 +2,26 @@ import React from "react"
 import '../Stylesheet/Home.css'
 import Header from '../Componentes/Header'
 
+
 function Home(){
   return (
-    <div className="Home">
-      <Header />
-      <h1>Home</h1>
-    </div>
-  );
+
+      <div className="header">
+
+          <h1>SUMMIT LUXURY Hotels</h1>
+
+          <div className="contenedor-boton">
+              <button className="boton-uno"><span><strong> INGRESA PARA HACER TUS RESERVAS! </strong></span></button>
+
+          </div>
+
+
+      </div>
+
+
+
+
+      );
 }
 
 export default Home;
