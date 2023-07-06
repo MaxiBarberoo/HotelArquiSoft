69725ab690ca492ecdb19c8a6776233d37490062
Juanmairon <2100815@ucc.edu.ar>
2023-07-06 16:10:37 -0300

    Navegación del boton home

--- FrontHotel/src/Rutas/Home.jsx
+++ FrontHotel/src/Rutas/Home.jsx
@@ -1,14 +1,21 @@
 import React from "react"
 import '../Stylesheet/Home.css'
 import Header from '../Componentes/Header'
+import { useNavigate } from "react-router-dom"
 
 function Home() {
+    const navigate = useNavigate();
+    
+    const handleButtonSubmit = () =>{
+        navigate('/loginandregister');
+    }
+
     return(
         <div className="header">
             <h1>SUMMIT LUXURY Hotels</h1>
-            <div className="contenedor-boton">
-                <button className="boton-uno"><span><strong> INGRESA PARA HACER TUS RESERVAS! </strong></span></button>
-            </div>
+            <form onSubmit={handleButtonSubmit} className="contenedor-boton">
+                <button type="submit" className="boton-uno"><span><strong>INGRESA PARA HACER TUS RESERVAS!</strong></span></button>
+            </form>
             <div className="contenedor-hoteles">
                 <div className="contenedor-detalle-hoteles">
                     <p className="nombre-hotel1"></p>
