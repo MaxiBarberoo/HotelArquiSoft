33a821bda2cc7498d0672f82930cc2c2229ce423
Juanmairon <2100815@ucc.edu.ar>
2023-07-06 17:21:18 -0300

    Prueba Hoteles

--- FrontHotel/src/Rutas/Home.jsx
+++ FrontHotel/src/Rutas/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import '../Stylesheet/Home.css'
 import Header from '../Componentes/Header'
 import { useNavigate } from "react-router-dom"
@@ -17,15 +17,14 @@ function Home() {
             .then((response) => response.json())
             .then((data) => setHoteles(data))
             .catch((error) => console.error(error));
-
-    })
+    }, []);
 
     return (
         <div className="header">
             <h1>SUMMIT LUXURY Hotels</h1>
-            <form onSubmit={handleButtonSubmit} className="contenedor-boton">
+            <forms onSubmit={handleButtonSubmit} className="contenedor-boton">
                 <button type="submit" className="boton-uno"><span><strong>INGRESA PARA HACER TUS RESERVAS!</strong></span></button>
-            </form>
+            </forms>
             <div className="contenedor-de-hoteles">
                 {hoteles.map((hotel) => (
                     <Hoteles
