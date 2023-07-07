76bee024069159c4d0f06781dd322e94759c5780
Juanmairon <2100815@ucc.edu.ar>
2023-07-07 19:47:49 -0300

    Home como debería andar

--- FrontHotel/src/Rutas/Home.jsx
+++ FrontHotel/src/Rutas/Home.jsx
@@ -1,70 +1,55 @@
-import React, { useState, useEffect } from "react"
-import '../Stylesheet/Home.css'
-import Header from '../Componentes/Header'
-import Hoteles from '../Componentes/Hoteles'
-import { useNavigate } from "react-router-dom"
+import React, { useState, useEffect } from "react";
+import '../Stylesheet/Home.css';
+import Header from '../Componentes/Header';
+import Hoteles from '../Componentes/Hoteles';
 
 function Home() {
+  const [hoteles, setHoteles] = useState([]);
 
-    const [hoteles, setHoteles] = useState([]);
-    const navigate = useNavigate();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:8090/hotels');
+        
+        if (response.ok) {
+          const data = await response.json();
+          setHoteles(data);
+        } else {
+          throw new Error('Error en la petición GET');
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-    const handleButtonSubmit = () => {
-        navigate('/loginandregister');
-    }
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('http://localhost:8090/hotels', {
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                });
-                if (response.ok) {
-                    const data = await response.json();
-                    setHoteles(data);
-                } else {
-                    throw new Error('Error en la petición GET');
-                }
-            } catch (error) {
-                console.error(error);
-            }
-        };
-
-        fetchData();
-    }, []);
-
-    return (
-        <div>
-            <Header />
-            <form onSubmit={handleButtonSubmit} className="contenedor-boton">
-                <button type="submit" className="boton-uno">
-                    <span>
-                        <strong>INGRESA PARA HACER TUS RESERVAS!</strong>
-                    </span>
-                </button>
-            </form>
-            <div className="contenedor-hoteles">
-                <div className="contenedor-detalle-hoteles">
-                    <p className="nombre-hotel1"></p>
-                </div>
-                <div className="contenedor-de-hoteles">
-                    <Hoteles />
-                    <div className="contenedor-de-hoteles">
-                        {hoteles.map((hotel) => (
-                            <Hoteles
-                                key={hotel.id}
-                                nombreHotel={hotel.name}
-                                piezas={hotel.cantHabitaciones}
-                                descripcion={hotel.descripcion}
-                            />
-                        ))}
-                    </div>
-                </div>
-            </div>
+  return (
+    <div>
+      <Header />
+      <div className="contenedor-hoteles">
+        <div className="contenedor-detalle-hoteles">
+          <p className="nombre-hotel1"></p>
         </div>
-    );
+        <div className="contenedor-de-hoteles">
+          <Hoteles />
+          <div className="contenedor-de-hoteles">
+            {hoteles.map((hotel) => (
+              <Hoteles
+                key={hotel.id}
+                nombreHotel={hotel.name}
+                piezas={hotel.cantHabitaciones}
+                descripcion={hotel.descripcion}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
+
 export default Home;
+
+
