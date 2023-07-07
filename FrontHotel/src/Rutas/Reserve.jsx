97b7dce058677dd1620a026c7aaa725455482576
Juanmairon <2100815@ucc.edu.ar>
2023-07-07 16:09:26 -0300

    Reserva visto

--- FrontHotel/src/Rutas/Reserve.jsx
+++ FrontHotel/src/Rutas/Reserve.jsx
@@ -4,8 +4,7 @@ import Header from '../Componentes/Header'
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
-function Reserve(){
-
+function Reserve() {
     const [fechaDesde, setFechaDesde] = useState(null);
     const [fechaHasta, setFechaHasta] = useState(null);
     const [hotelesDisponibles, setHotelesDisponibles] = useState([]);
@@ -40,46 +39,45 @@ function Reserve(){
                 })
                 .catch((error) => console.error(error));
         } else {
-            alert("Por favor, seleccione las fechas desde y hasta.");
+            if (fechaDesde > fechaHasta) {
+                alert("La fecha de checkin no puede ser mayor a la de checkout.");
+            } else {
+                alert("Por favor, seleccione las fechas desde y hasta.");
+            }
         }
     };
 
-  return (
-      <div>
-          <div className="header">
-              <h1>SUMMIT LUXURY Hotels</h1>
-          </div>
-
-          <div>
-              <br></br>
-              <h2>INGRESE LAS FECHAS para su estadia</h2>
-              <div>
-                  <p>Desde: </p>
-                  <DatePicker selected={fechaDesde} onChange={handleFechaDesdeChange} />
-              </div>
-              <div>
-                  <p>Hasta: </p>
-                  <DatePicker selected={fechaHasta} onChange={handleFechaHastaChange} />
-              </div>
-              <button onClick={buscarHotelesDisponibles}>BUSCAR</button>
-          </div>
-
-    {hotelesDisponibles.length > 0 && (
+    return (
         <div>
-            <h2>Hoteles Disponibles:</h2>
-            <ul>
-                {hotelesDisponibles.map((hotel) => (
-                    <li key={hotel.id}>
-                        <p>Nombre: {hotel.nombre}</p>
-                        <p>Cantidad de Habitaciones: {hotel.cantidadHabitaciones}</p>
-                    </li>
-                ))}
-            </ul>
+            <Header />
+            <div>
+                <br></br>
+                <h2>INGRESE LAS FECHAS para su estadia</h2>
+                <div>
+                    <p>Desde: </p>
+                    <DatePicker selected={fechaDesde} onChange={handleFechaDesdeChange} />
+                </div>
+                <div>
+                    <p>Hasta: </p>
+                    <DatePicker selected={fechaHasta} onChange={handleFechaHastaChange} />
+                </div>
+                <button onClick={buscarHotelesDisponibles}>BUSCAR</button>
+            </div>
+            {hotelesDisponibles.length > 0 && (
+                <div>
+                    <h2>Hoteles Disponibles:</h2>
+                    <ul>
+                        {hotelesDisponibles.map((hotel) => (
+                            <li key={hotel.id}>
+                                <p>Nombre: {hotel.nombre}</p>
+                                <p>Cantidad de Habitaciones: {hotel.cantidadHabitaciones}</p>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
         </div>
-    )}
-</div>
-
-  );
+    );
 }
 
 export default Reserve;
