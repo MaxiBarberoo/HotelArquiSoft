631d5c6819c3ba738af2dd51a76d508c7a308fc6
Maximo Barbero <maximobarbero@Maximos-MacBook-Pro.local>
2023-07-18 13:46:37 -0300

    Dockerizado

--- HotelArquiBack/model/amenitiehotel.go
+++ HotelArquiBack/model/amenitiehotel.go
@@ -3,7 +3,7 @@ package model
 type AmenitieHotel struct {
 	ID         int `gorm:"primaryKey"`
 	AmenitieId int `gorm:"foreignKey"`
-	HotelId    int `gorm:"foreignKey"`
+	HotelId    int `gorm:"foreignKey;not null; unique"`
 }
 
 type AmenitiesHotels []AmenitieHotel
